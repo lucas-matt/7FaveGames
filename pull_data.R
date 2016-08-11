f533de7da4f5bd9ed4ada18baaea80676a8ee83a
lucas-matt <lucasmatthew@hotmail.com>
2016-08-11 22:18:26 +0100

    Extracted good sample of 7favegames data

--- pull_data.R
+++ pull_data.R
@@ -1,6 +1,7 @@
 #install.packages("twitteR")
 
 library(twitteR)
+library(plyr)
 
 # expected R file called credentials.R with :
 # consumer_key = "..."
@@ -10,12 +11,21 @@ library(twitteR)
 source("credentials.R")
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
+batch <- 500
+pull <- TRUE
+tweets <- searchTwitter("#7FaveGames", n=batch, retryOnRateLimit=batch)
+parsed <- do.call("rbind.fill", lapply(tweets, as.data.frame))
+write.table(parsed, file="7favegames.csv", sep=",", append=TRUE, col.names=TRUE)
 
-all <- searchTwitter("#7FaveGames", n=6)
-res <- searchTwitter("#7FaveGames", n=3)
-id <- res[[3]]$id
-res2 <- searchTwitter("#7FaveGames", n=3, maxID=id)
-all
-res
-res2
+while (pull) {
+  id <- parsed$id[batch]
+  print(c("Getting batch with id ", id))
+  tweets <- searchTwitter("#7FaveGames", n=batch+1, retryOnRateLimit=batch, maxID=id)
+  parsed <- do.call("rbind.fill", lapply(tweets, as.data.frame))
+  parsed <- parsed[-(1:1),]
+  write.table(parsed, file="7favegames.csv", sep=",", append=TRUE, col.names=FALSE)
+  pull <- (nrow(parsed) > 1) || (!is.na(id)) 
+  Sys.sleep(10)
+}
+
 
